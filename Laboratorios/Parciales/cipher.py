7184d66c3a183a4ca19eb3716dadc2931c7e0218
cmarquezvz <carlosmarquezvz@gmail.com>
2015-07-06 21:13:00 -0500

    PARCIAL (PROF)

---
+++ Laboratorios/Parciales/cipher.py
@@ -0,0 +1,28 @@
+print ("1.\tIntroducir texto")
+print ("2.\tCifrar Texto")
+print ("3.\tDescifrar texto")
+print ("4.\tImprimir texto")
+
+op=0
+
+
+while op<5: # Indicamos que si el número ingresado es menor que 5, se ejecutará una acción
+       
+		op=int(input("Ingrese su opcion a elegir\n"))
+
+		if op==1:
+        	  	a=char(input("Ingrese un texto)) # Ingresamos texto que el usuario desee
+       
+		elif op==2:
+                        a=char(input"ingrese un texto"))
+		        ord("a")   # Haciendo que retorne a entero
+		        valor=a+1
+		        chr(valor)
+		elif op==3:
+                  	a=char(input"ingrese un texto"))
+		  	ord("a")  # Haciendo que retorne a cadena
+	          	valor=a-1
+	          	chr(valor)
+	
+		elif op==4:
+	          	print(a)
